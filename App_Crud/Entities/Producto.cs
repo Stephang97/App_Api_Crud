9960ceda1869d57f7b7f343a67e4aa9a1b43942c
Abraham <eas.gavilanez@yavirac,edu,ec>
2020-08-14 18:36:57 -0500

    Agregar archivos de proyecto.

---
+++ App_Crud/Entities/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Crud.Entities
+{
+    public class Producto
+    {
+        //datanotations
+        [Key]
+        public  string pro_codigo { get; set; }
+        public string pro_nombre { get; set; }
+        public string pro_descripcion { get; set; }
+        public string pro_precio { get; set; }
+
+    }
+}
